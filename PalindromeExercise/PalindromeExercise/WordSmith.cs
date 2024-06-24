2859bb7a3bc62c7f0ce75834cc4e6d5da44edf8c
Jesus Garcia <jgarciajr45@gmail.com>
2024-06-24 11:09:47 -0700

    Stanley Yelnats

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+		
+        public bool IsAPalindrome(string input)
+        {
+            var newInput = new string(input.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+            var revInput = new string(newInput.Reverse().ToArray());
+            return newInput == revInput;
+        }
+
+        
+    }
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,23 @@
-using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("banana", false)]
+        [InlineData("madam", true)]
+        [InlineData("Taco cat", true)]
+        public void IsAPalindromeTest(string input, bool expected)
         {
-
+            //Assert
+            var wordSmith = new WordSmith();
+            //Act
+            var actual = wordSmith.IsAPalindrome(input);
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
